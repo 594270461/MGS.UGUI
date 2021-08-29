e1cacbe2e993209cf96d9ca9cb62c3862ea3fddc
Mogoson <mogoson@outlook.com>
2021-08-29 15:57:39 +0800

    Add UIHTextRowFitter demo

---
+++ UnityProject/Assets/MGS.Packages/UGUI/Demo/Scripts/Widget/UIHTextRowFitterDemo.cs
@@ -0,0 +1,32 @@
+﻿/*************************************************************************
+ *  Copyright (c) 2021 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  UIHTextRowFitterDemo.cs
+ *  Description  :  Null.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  0.1.0
+ *  Date         :  8/29/2021
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using UnityEngine;
+
+namespace MGS.UGUI.Demo
+{
+    public class UIHTextRowFitterDemo : MonoBehaviour
+    {
+        public UIHTextRowFitter textRowFitter;
+
+        private void Start()
+        {
+            var datas = new HTextRowData[]
+            {
+                new HTextRowData("Test tittle:", "Test content"),
+                new HTextRowData("Test a long lengh tittle:", "Test content"),
+                new HTextRowData("Test tittle:", "Test a long lengh content")
+            };
+            textRowFitter.Refresh(datas);
+        }
+    }
+}
